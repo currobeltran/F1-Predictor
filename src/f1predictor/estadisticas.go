753d5aeaef3f2fbb0a8627f942a7e162081fe1b5
currobeltran <currob1999@gmail.com>
2020-10-04 15:55:43 +0200

    Añadiendo clase EstadísticasGP, close #8

---
+++ src/f1predictor/estadisticas.go
@@ -0,0 +1,11 @@
+package f1predictor
+
+type EstadisticasGP struct {
+	Accidentes        int
+	NumeroSafetyCar   int
+	Adelantamientos   int
+	BanderasAmarillas int
+	BanderasRojas     int
+	Sanciones         int
+	MejorVuelta       int
+}
