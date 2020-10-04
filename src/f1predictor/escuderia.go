b2f1121903d613a555a4471fb3320e730116932d
currobeltran <currob1999@gmail.com>
2020-10-04 15:54:31 +0200

    Añadiendo clase escuderia, close #12

---
+++ src/f1predictor/escuderia.go
@@ -0,0 +1,10 @@
+package f1predictor
+
+type Escuderia struct {
+	Nombre           string
+	Pilotos          [2]Piloto
+	TitulosMundiales int
+	Victorias        int
+	Poles            int
+	VueltasRapidas   int
+}
