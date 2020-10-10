bd3d04865ea4329bbc1e2c435a73c48de5d21838
currobeltran <currob1999@gmail.com>
2020-10-10 20:25:17 +0200

    Añadiendo get y set a SesionPiloto, close #25

--- src/f1predictor/sesion.go
+++ src/f1predictor/sesion.go
@@ -1,7 +1,59 @@
 package f1predictor
 
+/* Clase donde se relacionan los registros de un piloto en una determinada sesión. Los atributos
+ * son los siguientes:
+ *
+ *		- Tiempos que ha realizado el piloto en cada vuelta
+ * 		- Piloto con el que se relacionan los tiempos
+ *		- Mejor tiempo de la sesión realizado por el piloto
+ */
+
 type SesionPiloto struct {
-	Tiempos     []TiempoVuelta
-	Piloto      Piloto
-	MejorTiempo TiempoVuelta
+	tiempos     []TiempoVuelta
+	piloto      Piloto
+	mejorTiempo TiempoVuelta
+}
+
+type MetodosSesionPiloto interface {
+	Constructor(tiempos []TiempoVuelta, piloto Piloto, mejorTiempo TiempoVuelta)
+
+	GetTiempos() []TiempoVuelta
+	SetTiempos(tiempos []TiempoVuelta)
+
+	GetPiloto() Piloto
+	SetPiloto(piloto Piloto)
+
+	GetMejorTiempo() TiempoVuelta
+	SetMejorTiempo(mejorTiempo TiempoVuelta)
+}
+
+func (sp *SesionPiloto) Constructor(tiempos []TiempoVuelta, piloto Piloto,
+	mejorTiempo TiempoVuelta) {
+	sp.mejorTiempo = mejorTiempo
+	sp.piloto = piloto
+	sp.tiempos = tiempos
+}
+
+func (sp SesionPiloto) GetTiempos() []TiempoVuelta {
+	return sp.tiempos
+}
+
+func (sp SesionPiloto) SetTiempos(tiempos []TiempoVuelta) {
+	sp.tiempos = tiempos
+}
+
+func (sp SesionPiloto) GetPiloto() Piloto {
+	return sp.piloto
+}
+
+func (sp SesionPiloto) SetPiloto(piloto Piloto) {
+	sp.piloto = piloto
+}
+
+func (sp SesionPiloto) GetMejorTiempo() TiempoVuelta {
+	return sp.mejorTiempo
+}
+
+func (sp SesionPiloto) SetMejorTiempo(mejorTiempo TiempoVuelta) {
+	sp.mejorTiempo = mejorTiempo
 }
