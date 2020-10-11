67bf505c8cd58c32c8ffec0a6312ced3e9c1a5e9
currobeltran <currob1999@gmail.com>
2020-10-11 13:37:34 +0200

    Añadiendo metodo VerDatosPiloto, close #26

--- src/f1predictor/piloto.go
+++ src/f1predictor/piloto.go
@@ -1,5 +1,7 @@
 package f1predictor
 
+import "strconv"
+
 /*
  * Clase que contiene información sobre un piloto de Fórmula 1. Los atributos son los siguientes:
  *
@@ -35,6 +37,8 @@ type MetodosPiloto interface {
 
 	GetMundiales() int
 	SetMundiales(mundiales int)
+
+	VerDatosPiloto() string
 }
 
 func (p *Piloto) Constructor(nombre string, victorias int,
@@ -46,42 +50,79 @@ func (p *Piloto) Constructor(nombre string, victorias int,
 	p.mundiales = mundiales
 }
 
-func (p *Piloto) GetNombre() string {
+func (p Piloto) GetNombre() string {
 	return p.nombre
 }
 
-func (p *Piloto) SetNombre(nombre string) {
+func (p Piloto) SetNombre(nombre string) {
 	p.nombre = nombre
 }
 
-func (p *Piloto) GetVictorias() int {
+func (p Piloto) GetVictorias() int {
 	return p.victorias
 }
 
-func (p *Piloto) SetVictorias(victorias int) {
+func (p Piloto) SetVictorias(victorias int) {
 	p.victorias = victorias
 }
 
-func (p *Piloto) GetPoles() int {
+func (p Piloto) GetPoles() int {
 	return p.poles
 }
 
-func (p *Piloto) SetPoles(poles int) {
+func (p Piloto) SetPoles(poles int) {
 	p.poles = poles
 }
 
-func (p *Piloto) GetVueltasRapidas() int {
+func (p Piloto) GetVueltasRapidas() int {
 	return p.vueltasRapidas
 }
 
-func (p *Piloto) SetVueltasRapidas(vueltasRapidas int) {
+func (p Piloto) SetVueltasRapidas(vueltasRapidas int) {
 	p.vueltasRapidas = vueltasRapidas
 }
 
-func (p *Piloto) GetMundiales() int {
+func (p Piloto) GetMundiales() int {
 	return p.mundiales
 }
 
-func (p *Piloto) SetMundiales(mundiales int) {
+func (p Piloto) SetMundiales(mundiales int) {
 	p.mundiales = mundiales
+}
+
+/*
+ * Método que crea un string para ver de manera ordenada la información correspondiente
+ * a un piloto en concreto. Se da la opción de seleccionar cual es la información que se
+ * quiere obtener sobre el mismo.
+ */
+
+func (p Piloto) VerDatosPiloto(n bool, v bool, pol bool, vr bool, m bool) string {
+	var ret string
+
+	if n {
+		var nombre string = ("Nombre del piloto: " + p.nombre + "\n")
+		ret += nombre
+	}
+
+	if v {
+		var victorias string = ("Victorias conseguidas: " + strconv.Itoa(p.victorias) + "\n")
+		ret += victorias
+	}
+
+	if pol {
+		var poles string = ("Poles conseguidas: " + strconv.Itoa(p.poles) + "\n")
+		ret += poles
+	}
+
+	if vr {
+		var vueltasRapidas string = ("Vueltas rápidas conseguidas: " + strconv.Itoa(p.vueltasRapidas) + "\n")
+		ret += vueltasRapidas
+	}
+
+	if m {
+		var mundiales string = ("Mundiales conseguidos: " + strconv.Itoa(p.mundiales) + "\n")
+		ret += mundiales
+	}
+
+	return ret
 }
