d8c95b25604addc8c0f3225e76aa622c3790604c
currobeltran <currob1999@gmail.com>
2020-11-04 19:58:05 +0100

    Añadida funcion PosibilidadAccidentes, close #59

--- src/f1predictor/circuito.go
+++ src/f1predictor/circuito.go
@@ -232,6 +232,20 @@ func (c Circuito) PosibilidadBanderaAmarilla() float64 {
 //disputado en un determinado circuito
 func (c Circuito) PosibilidadAccidentes() float64 {
 	var ret float64 = 0.0
+	var suma float64 = 0.0
+	var div float64 = 0.0
+	var ponderacion float64 = 1.0
+
+	for i := len(c.resultados) - 1; i >= 0; i-- {
+		if ponderacion > 0 {
+			var est EstadisticasGP = c.resultados[i].GetEstadisticas()
+			suma += (float64(est.GetAccidentes()) * ponderacion)
+			div += ponderacion
+			ponderacion -= 0.05
+		}
+	}
+
+	ret = suma / div
 
 	return ret
 }
