25a2e708bbb3362bc62f3da65c5b668c1c6b0073
currobeltran <currob1999@gmail.com>
2020-12-04 13:32:52 +0100

    Añadiendo recursoEstadisticas, avanzando #72

---
+++ src/api/recursoEstadisticas.go
@@ -0,0 +1,6 @@
+package api
+
+//RecursoEstadisticas : Struct que representará un recurso relacionado con las estadísticas
+//de un GP
+type RecursoEstadisticas struct {
+}
--- src/main.go
+++ src/main.go
@@ -9,10 +9,12 @@ import (
 func main() {
 	circuito := new(recursos.RecursoCircuito)
 	escuderia := new(recursos.RecursoEscuderia)
+	estadistica := new(recursos.RecursoEstadisticas)
 	api := pastis.NewAPI()
 
 	api.AddResource("/api/circuito/:nombre", circuito)
 	api.AddResource("/api/escuderia/:nombre", escuderia)
+	api.AddResource("/api/estadisticas/:nombreCircuito/:temporada", estadistica)
 
 	api.Start(8080)
 }
