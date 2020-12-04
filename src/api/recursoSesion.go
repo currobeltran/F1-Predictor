45a4a4a216d6324b5c4f67c41405422eccbc5853
currobeltran <currob1999@gmail.com>
2020-12-04 14:04:31 +0100

    Añadiendo recursoSesion, avanzando #72

---
+++ src/api/recursoSesion.go
@@ -0,0 +1,5 @@
+package api
+
+//RecursoSesion : Struct que representará el recurso relacionado con una sesión de un GP
+type RecursoSesion struct {
+}
--- src/main.go
+++ src/main.go
@@ -11,12 +11,14 @@ func main() {
 	escuderia := new(recursos.RecursoEscuderia)
 	estadistica := new(recursos.RecursoEstadisticas)
 	piloto := new(recursos.RecursoPiloto)
+	sesion := new(recursos.RecursoSesion)
 	api := pastis.NewAPI()
 
 	api.AddResource("/api/circuito/:nombre", circuito)
 	api.AddResource("/api/escuderia/:nombre", escuderia)
-	api.AddResource("/api/estadisticas/:nombreCircuito/:temporada", estadistica)
+	api.AddResource("/api/gp/:nombreCircuito/:temporada/estadisticas", estadistica)
 	api.AddResource("/api/piloto/:nombre", piloto)
+	api.AddResource("/api/gp/:nombreCircuito/:temporada/sesion/:nombreSesion", sesion)
 
 	api.Start(8080)
 }
