44562a3685f8be4d13d175aaded3c39358b6067e
currobeltran <currob1999@gmail.com>
2020-12-10 11:06:24 +0100

    Añadiendo test ultimos metodos recursoEstadistica, close #73

--- src/main/main_test.go
+++ src/main/main_test.go
@@ -447,3 +447,45 @@ func TestEliminaEscuderia(t *testing.T) {
 			w.Body.String())
 	}
 }
+
+/****************************** OTROS TESTS *************************************/
+
+func TestObtieneMedia(t *testing.T) {
+	r := DiseñoRutas()
+
+	w := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/api/procesogp/australia/media/accidentes", nil)
+
+	r.ServeHTTP(w, request)
+
+	if w.Code != 200 {
+		t.Errorf("Código de estado no esperado, obtenido %d esperado %d", w.Code, 200)
+	}
+
+	matched, _ := regexp.MatchString("\"Media\":2", w.Body.String())
+
+	if !matched {
+		t.Errorf("Cuerpo de la petición incorrecto, obtenido %s, esperado que contuviese \"Media\":2",
+			w.Body.String())
+	}
+}
+
+func TestObtienePrediccion(t *testing.T) {
+	r := DiseñoRutas()
+
+	w := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/api/procesogp/australia/prediccion/accidentes", nil)
+
+	r.ServeHTTP(w, request)
+
+	if w.Code != 200 {
+		t.Errorf("Código de estado no esperado, obtenido %d esperado %d", w.Code, 200)
+	}
+
+	matched, _ := regexp.MatchString("\"Posibles Accidentes\":2", w.Body.String())
+
+	if !matched {
+		t.Errorf("Cuerpo de la petición incorrecto, obtenido %s, esperado que contuviese \"Posibles Accidentes\":2",
+			w.Body.String())
+	}
+}
